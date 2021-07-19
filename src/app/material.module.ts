0ff76d125e2fac600c98c2e61aca6c6c2de3dc33
routspin <routspin@gmail.com>
2021-07-19 18:26:39 +0200

    Basic toolbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     MatToolbarModule,
     MatMenuModule,
+    MatIconModule,
   ],
   exports: [
     MatToolbarModule,
     MatMenuModule,
     MatMenuTrigger,
+    MatIconModule,
   ]
 })
 export class MaterialModule { }
