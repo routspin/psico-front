9d964bfa6562129d4de81f23a167fff6e419b840
routspin <routspin@gmail.com>
2021-07-19 22:14:04 +0200

    Basic toolbar 02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,20 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from  './material.module';
 
 import { MenuComponent } from './menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [],
@@ -10,12 +12,14 @@ import { MatIconModule } from '@angular/material/icon';
     MatToolbarModule,
     MatMenuModule,
     MatIconModule,
+    BrowserAnimationsModule,
   ],
   exports: [
     MatToolbarModule,
     MatMenuModule,
     MatMenuTrigger,
     MatIconModule,
+    BrowserAnimationsModule,
   ]
 })
 export class MaterialModule { }
